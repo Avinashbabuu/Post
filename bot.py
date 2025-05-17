b759476d0c6089177c31e0e9aa0c396e68d02e9c
Avinashbabuu <158088631+Avinashbabuu@users.noreply.github.com>
2025-05-17 15:43:37 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN = "8013649463:AAHBlz-hW0YG6qM05pxQo4csKMye-asG2NY"
+TOKEN = "8013649463:AAFHV7OyF5P2SwiobmjO-hL4lYru1a8VjQc"
 bot = telebot.TeleBot(TOKEN)
 
 admin_id = 6484788124  # Replace with your Telegram ID
